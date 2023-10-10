d7dcb664e879027a1b163304a3eef4b86b9a5feb
Emily <emily.zy.lin@gmail.com>
2023-10-09 22:16:19 -0500

    Draft

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,19 +1,35 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+
+    @Size(min = 5, max = 15)
     private String username;
+
+    @Email(message = "invalid email format")
     private String email;
+
+    @Size(min = 6)
+    @NotBlank (message = "You must input a password here")
     private String password;
+
+    @Size(min = 6)
+    @NotBlank (message = "You must input a password here")
+    private String verifyPassword;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verifyPassword) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword = verifyPassword;
     }
 
     public String getUsername() {
@@ -38,6 +54,20 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+
+    private void checkPassword(){
+        if (!password.equals(verifyPassword)){
+            setVerifyPassword(null);
+        }
     }
 }
 
